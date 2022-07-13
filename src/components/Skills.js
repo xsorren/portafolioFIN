815c616b7840bff0c5cff7a7399649eb90cadbd1
xsorren <lauta.sorren@gmail.com>
2022-07-13 17:26:05 -0300

    sasd

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,7 +35,7 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Habilidades</h2>
-                        <p>Estas son los lenguajes/herramientas con los que trabajo</p>
+                        <p>Estos son los lenguajes/herramientas con los que trabajo</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                         <div className="item">
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
