db7cc225dc322e612c563c58277f5d102e90d13c
xsorren <lauta.sorren@gmail.com>
2022-07-17 17:50:18 -0300

    api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://git.heroku.com/polar-castle-44028.git/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
